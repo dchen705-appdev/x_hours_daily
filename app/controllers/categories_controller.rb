6b0ede1e51e54d96ff7a702b36aabd20360bc8c0
Raghu Betina <raghu@firstdraft.com>
2018-03-16 03:13:49 +0000

    Only category user can access show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :current_user_must_be_category_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_category_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_category_user
     category = Category.find(params[:id])
